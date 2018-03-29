f296cb92c6785a248a300a7cf772098d1a447d41
Andrew Chen <chen1458@purdue.edu>
2018-03-29 10:57:38 -0400

    Part 1 and 3 of q2 should be done?

--- lab4/one-level/apps/example/makeprocs/makeprocs.c
+++ lab4/one-level/apps/example/makeprocs/makeprocs.c
@@ -2,6 +2,7 @@
 #include "misc.h"
 
 #define HELLO_WORLD "hello_world.dlx.obj"
+#define PART2 "part2.dlx.obj"
 
 void main (int argc, char *argv[])
 {
@@ -19,22 +20,44 @@ void main (int argc, char *argv[])
   num_hello_world = dstrtol(argv[1], NULL, 10); // the "10" means base 10
   Printf("makeprocs (%d): Creating %d hello_world processes\n", getpid(), num_hello_world);
 
-  // Create semaphore to not exit this process until all other processes 
-  // have signalled that they are complete.
+  // Create semaphore to not exit this process until all other processes
+  // have signalled that they are complete
   if ((s_procs_completed = sem_create(0)) == SYNC_FAIL) {
     Printf("makeprocs (%d): Bad sem_create\n", getpid());
     Exit();
   }
 
-  // Setup the command-line arguments for the new processes.  We're going to
+  // Setup the command-line arguments for the new processes. We're going to
   // pass the handles to the semaphore as strings
   // on the command line, so we must first convert them from ints to strings.
   ditoa(s_procs_completed, s_procs_completed_str);
 
+  Printf("\n-------------------------------------Part1-------------------------------------------\n");
+  // Create Hello World processes
+  Printf("makeprocs (%d): Creating 1 hello world\n", getpid());
+  process_create(HELLO_WORLD, s_procs_completed_str, NULL);
+  if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
+    Printf("Bad semaphore s_procs_completed (%d) in %s\n", s_procs_completed, argv[0]);
+    Exit();
+  }
+  Printf("-------------------------------------------------------------------------------------\n");
+
+  Printf("\n-------------------------------------Part2-------------------------------------------\n");
+  process_create(PART2, s_procs_completed_str, NULL);
+  if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
+    Printf("Bad semaphore s_procs_completed (%d) in %s\n", s_procs_completed, argv[0]);
+    Exit();
+  }
+  Printf("-------------------------------------------------------------------------------------\n");
+
+  Printf("\n-------------------------------------Part3-------------------------------------------\n");
+  Printf("-------------------------------------------------------------------------------------\n");
+
+  Printf("\n-------------------------------------Part4-------------------------------------------\n");
   // Create Hello World processes
   Printf("-------------------------------------------------------------------------------------\n");
-  Printf("makeprocs (%d): Creating %d hello world's in a row, but only one runs at a time\n", getpid(), num_hello_world);
-  for(i=0; i<num_hello_world; i++) {
+  Printf("makeprocs (%d): Creating 100 hello world's in a row\n", getpid());
+  for(i=0; i<100; i++) {
     Printf("makeprocs (%d): Creating hello world #%d\n", getpid(), i);
     process_create(HELLO_WORLD, s_procs_completed_str, NULL);
     if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
@@ -42,8 +65,14 @@ void main (int argc, char *argv[])
       Exit();
     }
   }
-
   Printf("-------------------------------------------------------------------------------------\n");
+
+  Printf("\n-------------------------------------Part5-------------------------------------------\n");
+  Printf("-------------------------------------------------------------------------------------\n");
+
+  Printf("\n-------------------------------------Part6-------------------------------------------\n");
+  Printf("-------------------------------------------------------------------------------------\n");
+
   Printf("makeprocs (%d): All other processes completed, exiting main process.\n", getpid());
 
 }
--- lab4/one-level/apps/example/part2/part2.c
+++ lab4/one-level/apps/example/part2/part2.c
@@ -10,18 +10,17 @@ void main (int argc, char *argv[])
     Exit();
   } 
 
-
   // Convert the command-line strings into integers for use as handles
   s_procs_completed = dstrtol(argv[1], NULL, 10);
 
   // Now print a message to show that everything worked
-  Printf("hello_world (%d): Hello world!\n", getpid());
+  Printf("Accessing memory inside the virtual address space but outside of currently allocated pages: %d\n", getpid());
 
   // Signal the semaphore to tell the original process that we're done
   if(sem_signal(s_procs_completed) != SYNC_SUCCESS) {
-    Printf("hello_world (%d): Bad semaphore s_procs_completed (%d)!\n", getpid(), s_procs_completed);
+    Printf("part2 (%d): Bad semaphore s_procs_completed (%d)!\n", getpid(), s_procs_completed);
     Exit();
   }
 
-  Printf("hello_world (%d): Done!\n", getpid());
+  Printf("part2 (%d): Done!\n", getpid());
 }
