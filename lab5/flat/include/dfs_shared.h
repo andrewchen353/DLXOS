62853791b6974f54b9c11ccc26ac7976ae779f3d
Andrew Chen <chen1458@purdue.edu>
2018-04-06 17:23:13 -0400

    Tried to write dfs_shared.h and files_shared.h

--- lab5/flat/include/dfs_shared.h
+++ lab5/flat/include/dfs_shared.h
@@ -1,11 +1,18 @@
 #ifndef __DFS_SHARED__
 #define __DFS_SHARED__
 
+#define DFS_BLOCKSIZE  1024  // Must be an integer multiple of the disk blocksize
+#define NUM_ADDR_BLOCK 10
+#define FILENAME_SIZE  44
+
 typedef struct dfs_superblock {
   // STUDENT: put superblock internals here
+  uint32 valid;
+  uint32 fsBlocksize;       // DFS block size in bytes
+  uint32 numFsBlocks;       // total number of DFS blocks in file system
+  uint32 inodeStartBlock;   // starting block number of array of nodes
+  uint32 fbvStartBlock;     // starting block number of free block vector
 } dfs_superblock;
-
-#define DFS_BLOCKSIZE 1024  // Must be an integer multiple of the disk blocksize
 
 typedef struct dfs_block {
   char data[DFS_BLOCKSIZE];
@@ -13,10 +20,15 @@ typedef struct dfs_block {
 
 typedef struct dfs_inode {
   // STUDENT: put inode structure internals here
-  // IMPORTANT: sizeof(dfs_inode) MUST return 128 in order to fit in enough
+  // IMPORTANT: sizeof(dfs_inode) MUST return 96 in order to fit in enough
   // inodes in the filesystem (and to make your life easier).  To do this, 
   // adjust the maximumm length of the filename until the size of the overall inode 
   // is 128 bytes.
+  uint32 inuse;
+  uint32 fileSize;
+  char   filename[FILENAME_SIZE];
+  uint32 directAddr[NUM_ADDR_BLOCK];
+  uint32 indirectAddr;
 } dfs_inode;
 
 #define DFS_MAX_FILESYSTEM_SIZE 0x1000000  // 16MB
--- lab5/flat/include/files_shared.h
+++ lab5/flat/include/files_shared.h
@@ -5,12 +5,20 @@
 #define FILE_SEEK_END 2
 #define FILE_SEEK_CUR 3
 
-//#define FILE_MAX_FILENAME_LENGTH 76
+#define FILE_MAX_FILENAME_LENGTH 76
 
-//#define FILE_MAX_READWRITE_BYTES 4096
+#define FILE_MAX_READWRITE_BYTES 4096
 
 typedef struct file_descriptor {
   // STUDENT: put file descriptor info here
+  uint32     inuse;
+  char       filename[FILE_MAX_FILENAME_LENGTH];
+  //TODO check types below
+  dfs_inode* inode;
+  uint32     eof;
+  uint32     mode;
+  uint32     currentPosition;
+  uint32     processID;
 } file_descriptor;
 
 #define FILE_FAIL -1
